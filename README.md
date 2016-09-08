4868ace208e7394522886c5b6c5458b34093d239
Rohit Hazra <talk2rohithzr@gmail.com>
2016-09-09 00:09:14 +0530

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# rohithzr/nginx-php7
+
+This is a base [Docker image](https://www.docker.com/) to use as a web server with Nginx and PHP 7.
+Nginx and PHP are both preconfigured and ready to handle your connection.
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Default index</title>
+	<meta charset="utf-8">
+</head>
+<body style="font-family: Roboto, 'Helvetica Neue', 'Lucida Grande', sans-serif; background-color: #323232; color: white;">
+	<div style="margin: 3.5em auto 0; max-width: 420px; min-width: 240px; padding: 30px 35px; text-align: center">
+		<h3>Hello, </h3>
+		<p>Welcome to Our Site. Everything works great </p>
+		<p>But I think you did not upload a root index.php file, that file is supposed to replace me. :)</p>
+	</div>
+</body>
+</html>
